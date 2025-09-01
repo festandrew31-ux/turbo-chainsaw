eafc073b1d119d4be7a377305f2dd234b01b7ecb
festandrew31-ux <festandrew31@gmail.com>
2025-09-01 14:41:09 -0500

    Remove accidental turbo-chainsaw folder

--- turbo-chainsaw/week3/2025-08-26/commands.sh
+++
@@ -1,5 +0,0 @@
-mkdir ~/path-to-your-repo/week2
-mkdir: cannot create directory ‘/home/afest/path-to-your-repo/week2’: No such file or directory
-mkdir ~/cyber_rebuild/week2
-mkdir: cannot create directory ‘/home/afest/cyber_rebuild/week2/session_2025-08-26.txt’: File exists
-mv ~/path-to-your-repo/kali_full_setup.* ~/cyber_rebuild/week2/
