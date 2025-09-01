4830f7735f35b1c33d97baf5d9615d374681639a
festandrew31-ux <festandrew31@gmail.com>
2025-09-01 14:26:53 -0500

    Initial commit with week1 and repo structure

---
+++ turbo-chainsaw/week3/2025-08-26/commands.sh
@@ -0,0 +1,5 @@
+mkdir ~/path-to-your-repo/week2
+mkdir: cannot create directory ‘/home/afest/path-to-your-repo/week2’: No such file or directory
+mkdir ~/cyber_rebuild/week2
+mkdir: cannot create directory ‘/home/afest/cyber_rebuild/week2/session_2025-08-26.txt’: File exists
+mv ~/path-to-your-repo/kali_full_setup.* ~/cyber_rebuild/week2/
